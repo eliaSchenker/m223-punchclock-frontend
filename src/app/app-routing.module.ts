b2af62cfe8c4ac8d74953d0459b0daf6b99f42be
EliaSchenker <schenkere@bzz.ch>
2022-07-13 22:04:11 +0200

    Update app-routing.module.ts
    Added routes for various components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntryComponent } from './components/entry/entry.component';
+import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: OverviewComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'entries', component: EntryComponent},
+  //{path: 'admin/categories', component: },
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
