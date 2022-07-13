425fbd4063b13fe9f6a115a472786aa7baa36d3b
EliaSchenker <schenkere@bzz.ch>
2022-07-13 22:02:20 +0200

    Add Delete Dialog
    Added a Dialog which helps with the deletion of any object

---
+++ src/app/components/dialogs/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-delete-dialog',
+  templateUrl: './confirm-delete-dialog.component.html',
+  styleUrls: ['./confirm-delete-dialog.component.css']
+})
+export class ConfirmDeleteDialogComponent implements OnInit {
+  title: string;
+
+  constructor(public dialogRef: MatDialogRef<ConfirmDeleteDialogComponent>) {
+    this.title = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+}
