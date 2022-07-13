5f384837a9098edbee48fcace3d66f817b3cb653
EliaSchenker <schenkere@bzz.ch>
2022-07-13 21:56:51 +0200

    Add LoginComponent
    Added the LoginComponent which contains a Login Form

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form:FormGroup;
+
+  constructor(private fb:FormBuilder, private authService: AuthService) {
+    this.form = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    const val = this.form.value;
+    if (val.username && val.password) {
+        this.authService.login(val.email, val.password);
+    }
+  }
+}
