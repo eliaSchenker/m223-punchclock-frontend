f479d627c98a47cc6a3d46ef26b3338a4d9daa30
EliaSchenker <schenkere@bzz.ch>
2022-07-14 20:29:32 +0200

    Finish UserDialog
    UserDialog now completely functional

--- src/app/components/dialogs/create-update-user-dialog/create-update-user-dialog.component.ts
+++ src/app/components/dialogs/create-update-user-dialog/create-update-user-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Location } from 'src/app/model/Location';
 import { User } from 'src/app/model/User';
+import { LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'app-create-update-user-dialog',
@@ -10,13 +12,24 @@ import { User } from 'src/app/model/User';
 export class CreateUpdateUserDialogComponent implements OnInit {
   user: User;
   action: string;
+  locations: Location[];
+  selected_location_id: number;
 
-  constructor(public dialogRef: MatDialogRef<CreateUpdateUserDialogComponent>) {
-    this.user = {id: undefined, username: "", password: "", role: "User"};
+  constructor(public dialogRef: MatDialogRef<CreateUpdateUserDialogComponent>,
+    private locationService: LocationService) {
+    this.user = {id: undefined, username: "", password: "", role: "User", location: {id: undefined, name:""}};
     this.action = "Add";
+    this.locations = [];
+    this.selected_location_id = 1;
   }
 
   ngOnInit(): void {
+    this.locationService.getLocations().subscribe((locations) => {
+      this.locations = locations;
+      if(this.user.location.id != undefined) {
+        this.user.location = this.locations.filter((e) => e.id === this.user.location.id)[0];
+      }
+    });
   }
 
   onConfirm(): void {
