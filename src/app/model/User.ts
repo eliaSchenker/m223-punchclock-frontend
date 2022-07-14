87ecffe351c9e6073a8899d5a7d46b0ae6782864
EliaSchenker <schenkere@bzz.ch>
2022-07-14 19:16:33 +0200

    Update User.ts
    Add password, role and location to the user model

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,6 +1,9 @@
+import { Location } from "./Location";
+
 export interface User {
   id?: number,
   username: string;
-  password: string;
-  role:string;
+  password?: string;
+  role: string;
+  location?: Location;
 }
