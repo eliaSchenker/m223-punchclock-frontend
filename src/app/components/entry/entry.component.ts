2a0362bce7d3db6b8dd7e1874e9bb501e696045a
EliaSchenker <schenkere@bzz.ch>
2022-07-14 20:00:09 +0200

    Add EntryComponent Functionality
    Added Functionality to the Entry Component (adding, editing, deleting)

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Entry } from 'src/app/model/Entry';
+import { EntryService } from 'src/app/services/entry.service';
+import { ConfirmDeleteDialogComponent } from '../dialogs/confirm-delete-dialog/confirm-delete-dialog.component';
+import { CreateUpdateEntryDialogComponent } from '../dialogs/create-update-entry-dialog/create-update-entry-dialog.component';
 
 @Component({
   selector: 'app-entry',
@@ -6,10 +12,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entry.component.css']
 })
 export class EntryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  entries: Entry[];
+  displayedColumns: string[] = ['id', 'checkIn', 'checkOut', 'category', 'update', 'delete'];
+  constructor(private entryService: EntryService,
+    private router: Router,
+    private dialog: MatDialog) {
+    this.entries = [];
   }
 
+  ngOnInit(): void {
+    this.entryService.getEntries().subscribe((entries) => {
+      this.entries = entries;
+    })
+  }
+
+  back() {
+    this.router.navigateByUrl('')
+  }
+
+  addEntry() {
+    const dialogRef = this.dialog.open(CreateUpdateEntryDialogComponent)
+    dialogRef.componentInstance.action = "Add";
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.entryService.addEntry(result).subscribe(() => {
+          window.location.reload();
+        }, () => {
+          //TODO Error handling
+        });
+      }
+    });
+  }
+
+  updateEntry(entry: Entry) {
+    const dialogRef = this.dialog.open(CreateUpdateEntryDialogComponent)
+    dialogRef.componentInstance.entry = JSON.parse(JSON.stringify(entry));
+    dialogRef.componentInstance.action = "Edit";
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.entryService.updateEntry(result).subscribe(() => {
+          window.location.reload();
+        }, () => {
+          //TODO Error handling
+        });
+      }
+    });
+  }
+
+  deleteEntry(id: number) {
+    const dialogRef = this.dialog.open(ConfirmDeleteDialogComponent)
+    dialogRef.componentInstance.title = "Confirm delete";
+    dialogRef.componentInstance.text = "Are you sure that you want to delete the entry with the id " + id + "?";
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.entryService.deleteEntry(id).subscribe(() => {
+          window.location.reload();
+        }, () => {
+          //TODO Error handling
+        });
+      }
+    });
+  }
 }
