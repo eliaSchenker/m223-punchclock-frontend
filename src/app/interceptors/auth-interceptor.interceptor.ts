afbc53b09a294c1d4ed9c9c1c6f17241d28fc681
EliaSchenker <schenkere@bzz.ch>
2022-07-14 19:17:17 +0200

    Update auth-interceptor.interceptor.ts
    Update auth-interceptor-interceptor to automatically redirect to login if a unauthorized status is returned by the API

--- src/app/interceptors/auth-interceptor.interceptor.ts
+++ src/app/interceptors/auth-interceptor.interceptor.ts
@@ -4,19 +4,23 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpErrorResponse,
+  HttpStatusCode,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 /**
  * Interceptor for any HTTP Requests going to the API (Adds the Authentication Bearer Token)
  * Source: https://blog.angular-university.io/angular-jwt-authentication/
  */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let accessToken = sessionStorage.getItem("access_token");
+    let accessToken = localStorage.getItem("token");
     if(accessToken)
     {
         request = request.clone({
@@ -25,6 +29,18 @@ export class AuthInterceptor implements HttpInterceptor {
         }
         });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        return event;
+      }),
+      catchError((httpErrorResponse: HttpErrorResponse, _: Observable<HttpEvent<any>>) => {
+          if (httpErrorResponse.status === HttpStatusCode.Unauthorized) {
+            this.authService.logout();
+            this.router.navigateByUrl('login');
+          }
+          return throwError(httpErrorResponse);
+        }
+      )
+    );
   }
 }
