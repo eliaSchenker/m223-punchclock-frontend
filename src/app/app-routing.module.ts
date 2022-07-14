1593b4329a2d20402eb52f5d8765388b7aab543c
EliaSchenker <schenkere@bzz.ch>
2022-07-14 20:30:18 +0200

    Update app-routing.module.ts
    Added routes to the administrator pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/admin/categories/categories.component';
+import { LocationsComponent } from './components/admin/locations/locations.component';
+import { UsersComponent } from './components/admin/users/users.component';
 import { EntryComponent } from './components/entry/entry.component';
 import { LoginComponent } from './components/login/login.component';
 import { OverviewComponent } from './components/overview/overview.component';
@@ -8,7 +11,9 @@ const routes: Routes = [
   {path: '', component: OverviewComponent},
   {path: 'login', component: LoginComponent},
   {path: 'entries', component: EntryComponent},
-  //{path: 'admin/categories', component: },
+  {path: 'admin/categories', component: CategoriesComponent},
+  {path: 'admin/locations', component: LocationsComponent},
+  {path: 'admin/users', component: UsersComponent},
   {path: '**', redirectTo: ''}
 ];
 
