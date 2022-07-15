38f1aa61224557ede30f28c52759b1786ce62f2e
EliaSchenker <schenkere@bzz.ch>
2022-07-15 11:09:57 +0200

    Update api_config.ts
    Add SaveType enum to api_config (used in the dialogs)

--- src/app/api_config.ts
+++ src/app/api_config.ts
@@ -1,9 +1,16 @@
 import { HttpHeaders } from "@angular/common/http";
 
 //Various configuration variables for the API integration
-export const API_URL:string = "http://localhost:8080/";
+export const API_URL:string = "http://localhost:8080/"; //API URL
+//Http Headers used in the requests
 export const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
+}
+
+//SaveType used in the dialogs
+export const enum SaveType {
+  Add = "Add",
+  Edit = "Edit",
 }
