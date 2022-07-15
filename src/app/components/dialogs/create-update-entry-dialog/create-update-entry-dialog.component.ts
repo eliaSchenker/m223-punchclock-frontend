055840596ca3eabcac7674b988ad0b075a5eee6f
EliaSchenker <schenkere@bzz.ch>
2022-07-15 07:44:24 +0200

    Finish CreateUpdateEntryDialog
    Finished the CreateUpdateEntryDialog and its functionality

--- src/app/components/dialogs/create-update-entry-dialog/create-update-entry-dialog.component.ts
+++ src/app/components/dialogs/create-update-entry-dialog/create-update-entry-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Category } from 'src/app/model/Category';
 import { Entry } from 'src/app/model/Entry';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-create-update-entry-dialog',
@@ -9,17 +10,24 @@ import { Entry } from 'src/app/model/Entry';
   styleUrls: ['./create-update-entry-dialog.component.css']
 })
 export class CreateUpdateEntryDialogComponent implements OnInit {
-  entry: Entry;
+  public entry: Entry;
   categories: Category[];
   action: string;
 
-  constructor(public dialogRef: MatDialogRef<CreateUpdateEntryDialogComponent>) {
-    this.entry = {id: undefined, checkIn: new Date(), checkout: new Date(), category: {id: 1, name: "Test"}}
+  constructor(public dialogRef: MatDialogRef<CreateUpdateEntryDialogComponent>,
+    private categoryService: CategoryService) {
+    this.entry = {id: undefined, checkIn: new Date(), checkOut: new Date(), category: {id: undefined, name: ""}}
     this.categories = [];
     this.action = "New";
   }
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((categories)=> {
+      this.categories = categories;
+      if(this.entry.category.id != undefined) {
+        this.entry.category = this.categories.filter((e) => e.id === this.entry.category.id)[0];
+      }
+    })
   }
 
   onConfirm(): void {
